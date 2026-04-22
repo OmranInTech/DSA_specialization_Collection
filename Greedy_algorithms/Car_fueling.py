ca5586707147cde47f5b835f1a20fd8bb58b02f2
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2026-04-22 21:46:15 +0430

    2 algorithms added

--- Greedy_algorithms/Car_fueling.py
+++ Greedy_algorithms/Car_fueling.py
@@ -2,28 +2,33 @@ def car_fueling():
     d = int(input())
     m = int(input())
     n = int(input())
-    
+
     stops = list(map(int, input().split()))
-    
+
+    # add start and destination
     stops = [0] + stops + [d]
-    
-    num_refills = 0
+
+    refills = 0
     current = 0
-    
+
     while current < len(stops) - 1:
         last = current
-        
-        while (current < len(stops) - 1 and 
+
+        # move as far as possible within fuel range
+        while (current < len(stops) - 1 and
                stops[current + 1] - stops[last] <= m):
             current += 1
-        
+
+        # if we cannot move at all → impossible
         if current == last:
             print(-1)
             return
-        
+
+        # if not yet at destination → refill
         if current < len(stops) - 1:
-            num_refills += 1
-    
-    print(num_refills)
+            refills += 1
+
+    print(refills)
+
 
 car_fueling()
